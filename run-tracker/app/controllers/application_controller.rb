428cf59b51a8bb8ff1bfd41951c47e9f9d262164
alexcoldbrew <alexcoldbrew@gmail.com>
2020-11-28 15:20:16 -0600

    updates routes

--- run-tracker/app/controllers/application_controller.rb
+++ run-tracker/app/controllers/application_controller.rb
@@ -12,31 +12,44 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/runs/new' do
-
+    erb :new
   end
 
   get '/runs' do
-
+    @runs = Run.all
+    erb :index
   end
 
   post '/runs' do
-
+    @run = Run.create(:time => params[:time], :distance => params[:distance], :hours => params[:hours], :minutes => params[:minutes], :seconds => params[:seconds])
+    redirect to "/runs/#{@run.id}"
   end
 
   get '/runs/:id' do
-
+    @run = Run.find_by_id(params[:id])
+    erb :show
   end
 
   get '/runs/:id/edit' do
-
+    @run = Run.find_by_id(params[:id])
+    erb :edit
   end
 
   patch '/runs/:id' do
-
+    @run = Run.find_by_id(params[:id])
+    @run.time = params[:time]
+    @run.distance = params[:distance]
+    @run.hours = params[:hours]
+    @run.minutes = params[:minutes]
+    @run.seconds = params[:seconds]
+    @run.save
+    redirect to "/runs/#{@run.id}"
   end
 
   delete '/runs/:id' do
-
+    @run = Run.find_by_id(params[:id])
+    @run.delete
+    redirect to "/runs"
   end
 
 end
