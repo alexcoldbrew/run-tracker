ac8203257894a992de6577b553f983a69db21eb4
alexcoldbrew <alexcoldbrew@gmail.com>
2020-11-27 18:10:16 -0600

    created migrations and migrated the tables

---
+++ run-tracker/db/migrate/20201127235730_create_runs.rb
@@ -0,0 +1,12 @@
+class CreateRuns < ActiveRecord::Migration
+  def change
+    create_table :runs do |t|
+      t.datetime :time
+      t.float :distance
+      t.integer :hours
+      t.integer :minutes
+      t.integer :seconds
+      # will be able to convert hours to minutes and determine average pace for the distance I believe
+    end
+  end
+end
