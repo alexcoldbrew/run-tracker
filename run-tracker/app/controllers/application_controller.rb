051c723798c049a03639255d82c214e2938ba58e
alexcoldbrew <alexcoldbrew@gmail.com>
2020-11-28 17:59:03 -0600

    added some routes and view files, need to elaborate on them.

--- run-tracker/app/controllers/application_controller.rb
+++ run-tracker/app/controllers/application_controller.rb
@@ -25,6 +25,29 @@ class ApplicationController < Sinatra::Base
     redirect to "/runs/#{@run.id}"
   end
 
+  post '/login' do
+    @user = User.find_by_id(:username => params[:username])
+    if @user != nil && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect to '/account'
+    end
+    erb :error
+  end
+
+  get '/account' do
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb:account
+    else
+      erb :error
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect to '/'
+  end
+
   get '/runs/:id' do
     @run = Run.find_by_id(params[:id])
     erb :show
