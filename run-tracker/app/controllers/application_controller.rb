aed5d36993aa55e3a9708710877048a412c84a35
alexcoldbrew <alexcoldbrew@gmail.com>
2020-12-01 21:47:09 -0600

    fixed login and sign up routes

--- run-tracker/app/controllers/application_controller.rb
+++ run-tracker/app/controllers/application_controller.rb
@@ -23,7 +23,42 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/login' do
-    erb :login
+    if logged_in?
+      redirect to '/runs'
+    else
+      erb :login
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/runs'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/signup' do
+    if session[:id] == nil
+      erb :signup
+    else
+      redirect to '/runs'
+  end
+
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect to :signup
+    else
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:id] = @user.id
+      redirect to '/runs'
+  end
+
+  get '/logout' do
+    session.clear
+    redirect to '/login'
   end
 
 
