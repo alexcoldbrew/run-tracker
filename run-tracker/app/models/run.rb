5433eb8ddad0a6019480a07f75b8a15166e6e220
alexcoldbrew <alexcoldbrew@gmail.com>
2020-12-02 16:08:54 -0600

    added validations to models

--- run-tracker/app/models/run.rb
+++ run-tracker/app/models/run.rb
@@ -1,3 +1,8 @@
 class Run < ActiveRecord::Base
     belongs_to :user
+    validates :date, presence: true
+    validates :distance, presence: true
+    validates :hours, presence: true
+    validates :minutes, presence: true
+    validates :seconds, presence: true
 end
--- run-tracker/app/models/user.rb
+++ run-tracker/app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
     has_many :run
+    validates :username, presence: true
+    validates :email, presence: true
+    validates :password, presence: true
+    validates :username, uniqueness: true
 end
