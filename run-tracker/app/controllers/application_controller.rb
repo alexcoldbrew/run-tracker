ca1f32109a6abf098e5963a86e887c47759bbc52
alexcoldbrew <alexcoldbrew@gmail.com>
2020-12-09 14:22:38 -0600

    fixed weird bug with current user helper method and show page

--- run-tracker/app/controllers/application_controller.rb
+++ run-tracker/app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < Sinatra::Base
     end
 
     def current_user
-      @user ||= User.find(session[:user_id]) 
+      @current_user ||= User.find_by_id(session[:user_id])
     end
   end
 
